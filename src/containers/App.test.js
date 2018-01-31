b0cbea7832dad4506970843ab67c648fb22734ac
Nans <nans@gigan.fr>
2018-01-31 21:36:22 +0100

    update du soir

--- src/containers/App.test.js
+++ src/containers/App.test.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<MuiThemeProvider><App /></MuiThemeProvider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
