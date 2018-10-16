775561fec0ae55b62454f52cdb722bebb22b03df
Nans <nans.gigan@gmail.com>
2018-10-16 10:46:53 +0200

    update test

--- src/containers/App.test.js
+++ src/containers/App.test.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+
 import App from './App';
+import { store } from '../store'
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<MuiThemeProvider><App /></MuiThemeProvider>, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <MuiThemeProvider>
+        <App />
+      </MuiThemeProvider>
+    </Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
